511bdec88aa6317a2af5af42615fc1711a45622f
imHireki <imhireki@gmail.com>
2022-02-09 19:29:56 -0300

    [feat]: add WebSite constants

--- scraper.py
+++ scraper.py
@@ -43,12 +43,19 @@ class Browser:
 
 
 class WebSite:
-    """ Get elements on the HTML of the website """
+    """ Handle elements on the HTML of the website """
     ENDPOINT_SEARCH = 'https://www.pinterest.com/search/pins/?q='
+    ENDPOINT_HOME = 'https://br.pinterest.com'
 
     ELEMENT_LOGIN_BUTTON = 'div.Eqh:nth-child(3) > div:nth-child(1) > button:nth-child(1)'
     ELEMENT_EMAIL_INPUT = '#email'
     ELEMENT_PASSWORD_INPUT = '#password'
+
+    ELEMENT_PINS = 'div.vbI:nth-child(1)'
+
+    ELEMENT_TITLE = 'h1.lH1'
+    ELEMENT_SUBTITLE = 'div.FNs:nth-child(2) > span:nth-child(1)'
+    ELEMENT_IMAGES = '.OVX > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1)'
 
     def __init__(self, driver):
         self.driver = driver
