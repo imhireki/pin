599b65db97a44dc62c3cf10821987a7b0179fb23
imHireki <imhireki@gmail.com>
2022-02-16 08:54:43 -0300

    [feat]: add user-data-dir and timeout exception to avoid unecessary login

--- scraper.py
+++ scraper.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium import webdriver
@@ -27,6 +28,7 @@ class Browser:
     def _driver() -> ChromiumDriver:
         """ Return a configured ChromiumDriver """
         driver_options = webdriver.ChromeOptions()
+        driver_options.add_argument('user-data-dir=.selenium')
         driver_options.headless = False
         return webdriver.Chrome(options=driver_options)
 
@@ -226,10 +228,14 @@ class Client:
         """ Perform login using enviroment keys """
 
         # Go to the login form
-        login_button = self.site.web_element(
-            element=self.site.ELEMENT_LOGIN_BUTTON,
-            condition=EC.element_to_be_clickable,
+        try:
+            login_button = self.site.web_element(
+                element=self.site.ELEMENT_LOGIN_BUTTON,
+                condition=EC.element_to_be_clickable,
+                timeout=5.0,
             ).click()
+        except TimeoutException:  # User already logged in
+            return
 
         # Get the fields elements
         email_input = self.site.web_element(self.site.ELEMENT_EMAIL_INPUT)
