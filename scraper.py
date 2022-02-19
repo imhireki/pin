554bdf11dc61959fb5f5153022310e6f186be4d4
imHireki <imhireki@gmail.com>
2022-02-19 09:51:13 -0300

    [feat]: add SQL and insert to support databases

--- scraper.py
+++ scraper.py
@@ -182,6 +182,62 @@ class JsonFile:
         return json_data
 
 
+class SQL(ABC):
+    @abstractmehod
+    def __init__(self, connection:Dict[str, str]):
+        pass
+
+    def insert(self, data:Dict[str, list]):
+        """Insert the data inside db's tables
+
+        Args:
+            data: dictionary with url:str, title:str, subtitle:str, images:list
+        """
+
+        # inserting url title e subtitle
+        self.cursor.execute(
+            """
+            INSERT INTO
+                pins (url, title, subtitle)
+            VALUES
+                (%s, %s, %s)
+            ;
+            """,
+            (data['url'], data['title'], data['subtitle'])
+        )
+        self.connection.commit()  # maybe just one commit
+
+        # query the id from the last insertion
+        self.cursor.execute(
+            """
+            SELECT
+                pin.id
+            FROM
+                pins as pin
+            WHERE
+                pin.url = %s
+            ;
+            """,
+            [data['url']]
+        )
+        rows = self.cursor.fetchall()
+        pin_id = [col[0] for col in rows][0]
+
+        for image in data['images']:
+            # insert the image  FIXME: pins_id to pin_id or pin
+            self.cursor.execute(
+                """
+                INSERT INTO
+                    images (pins_id, url)
+                VALUES
+                    (%s, %s)
+                ;
+                """,
+                (pin_id, image)
+            )
+            self.connection.commit()  # maybe just one commit
+
+
 class Storage:
     @staticmethod
     def json(filename):
