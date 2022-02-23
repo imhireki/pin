83d624bf89a40b41453a2661ced5115d6262cd58
imHireki <imhireki@gmail.com>
2022-02-23 15:21:19 -0300

    [feat]: add class to handle the database connection

--- data/store/database.py
+++ data/store/database.py
@@ -4,18 +4,14 @@ import mysql.connector
 
 
 class SQL:
-    def __init__(self, connection:Dict[str, str]):
-        pass
-
     def insert(self, data:Dict[str, list]):
         """Insert the data inside db's tables
 
         Args:
             data: dictionary with url:str, title:str, subtitle:str, images:list
         """
-
         # inserting url title e subtitle
-        self.cursor.execute(
+        self.execute(
             """
             INSERT INTO
                 pins (url, title, subtitle)
@@ -25,10 +21,11 @@ class SQL:
             """,
             (data['url'], data['title'], data['subtitle'])
         )
-        self.connection.commit()  # maybe just one commit
+
+        self.commit()
 
         # query the id from the last insertion
-        self.cursor.execute(
+        self.execute(
             """
             SELECT
                 pin.id
@@ -40,12 +37,12 @@ class SQL:
             """,
             [data['url']]
         )
-        rows = self.cursor.fetchall()
+        rows = self.fetchall()
         pin_id = [col[0] for col in rows][0]
 
         for image in data['images']:
             # insert the image  FIXME: pins_id to pin_id or pin
-            self.cursor.execute(
+            self.execute(
                 """
                 INSERT INTO
                     images (pins_id, url)
@@ -55,7 +52,8 @@ class SQL:
                 """,
                 (pin_id, image)
             )
-            self.connection.commit()  # maybe just one commit
+
+            self.commit()
 
     def select(self, urls:List[str]):
         """Select urls matching `urls` sent in the list.
@@ -65,7 +63,7 @@ class SQL:
         Returns:
             list: List[str] if any matches, List[] otherwise.
         """
-        self.cursor.execute(
+        self.execute(
             """
             SELECT
                 pin.url
@@ -77,18 +75,56 @@ class SQL:
             """.format(('%s, ' * len(urls))[:-2]),  # %s for each url
             urls
         )
-        rows = self.cursor.fetchall()
+        rows = self.fetchall()
         return [col[0] for col in rows]
 
 
-class Postgres(SQL):
-    def __init__(self, connection:Dict[str, str]):
-        self.connection = psycopg.connect(**connection)
-        return super().__init__(self)
-        self.cursor = self.connection.cursor()
+class DBConnection:
+    def __init__(self, database:str, conn_params:Dict[str, Union[int, str]]):
+        self.connection = (database, conn_params)
+        self.cursor = self._connection
+
+    @property
+    def connection(self):
+        return self._connection
+
+    @connection.setter
+    def connection(self, conn):
+        try:
+            # Connection
+            if conn[0] == 'Postgres':
+                self._connection = psycopg.connect(**conn[1])
+            elif conn[0] == 'MySQL':
+                self._connection = mysql.connector.connect(conn[1])
+        except Exception as e:
+            print(e, '\n[!] Error connecting to the database [!]')
+            exit()
+
+    @property
+    def cursor(self):
+        return self._cursor
+
+    @cursor.setter
+    def cursor(self, connection):
+        if isinstance(connection, psycopg.Connection):
+            self._cursor = connection.cursor()
+
+    def fetchall(self):
+        return self._cursor.fetchall()
+
+    def execute(self, sql:str, params=()):
+        return self.cursor.execute(sql, params)
+
+    def commit(self):
+        return self._connection.commit()
 
 
-class MySQL(SQL):
-    def __init__(self, connection:Dict[str, str]):
-        self.connection = mysql.connector.connect(**connection)
-        self.cursor = self.connection.cursor()
+class Postgres(DBConnection, SQL):
+    def __init__(self, conn_params:Dict[str, str]):
+        DBConnection.__init__(self, self.__class__.__name__, conn_params)
+
+
+class MySQL(SQL, DBConnection):
+    def __init__(self, conn_params:Dict[str, str]):
+        DBConnection.__init__(self, self.__class__.__name__, conn_params)
+
