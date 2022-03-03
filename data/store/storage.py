2f56278ce72b75759e70b2b29370c14c38367c31
imHireki <imhireki@gmail.com>
2022-03-03 10:33:43 -0300

    [doc]: add documentation to the storage module

--- data/store/storage.py
+++ data/store/storage.py
@@ -1,12 +1,15 @@
-from .file import JsonFile
 from .database import Postgres, MySQL
+from .file import JsonFile
 
 
 def postgres(connection):
+    """Return a Postgres database storage instance."""
     return Postgres(connection)
 
 def mysql(connection):
+    """Return a MySQL database storage instance."""
     return MySQL(connection)
 
 def json(filename):
+    """Return a Json file storage instance."""
     return JsonFile(filename)
