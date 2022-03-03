4126a56292f1e31c650253b41f505a7ed37e50b1
imHireki <imhireki@gmail.com>
2022-03-03 18:30:41 -0300

    [doc/feat]: doc the file and add cursor to MySQL

--- data/store/database.py
+++ data/store/database.py
@@ -4,7 +4,17 @@ import mysql.connector
 
 
 class SQL:
-    def insert_into(self, table:str, column:Union[str, list], value:str):
+    def insert_into(self, table:str, column:Union[str, list], value):
+        """Perform a SQL insert statement.
+
+        INSERT INTO `table` (`column`) VALUES (`value`); .
+
+        Args:
+            table: The table to insert into.
+            column: The column to insert into.
+            value: The value to insert into the column.
+        """
+
         statement = 'INSERT INTO {} ({}) VALUES ({})'.format(
                         table,
                         (('{}, ' * len(column))[:-2]).format(*column),
@@ -13,12 +23,21 @@ class SQL:
         self.execute(statement, value)
         self.commit()
 
-    def select_from(self, table, column, query:Dict[str, str]):
-        """ Perform a SQL select statement
+    def select_from(self, table:str, column:str, query:Dict[str, str]):
+        """Perform a SQL select statement.
 
         SELECT `column` FROM `table`
-        WHERE `query['column']` IN `query['value']`
+        WHERE `query['column']` IN `query['value']`; .
+
+        Args:
+            table: The table to perform the query.
+            column: The column to perform the query.
+            query: The column and its value to query for.
+
+        Returns:
+            List[tuple]: The rows matching the query.
         """
+
         if type(query['value']) is str:
             query['value'] = [query['value']]
 
@@ -30,40 +49,45 @@ class SQL:
         self.execute(statement, query['value'])
         return self.fetchall()
 
-    def insert_pin(self, data:Dict[str, list]):
-        """Insert the data inside db's tables
+    def insert_pin(self, data:Dict[str, Union[str, list]]):
+        """Insert the data inside the database.
 
         Args:
-            data: dictionary with url:str, title:str, subtitle:str, images:list
+            data: A pin's data.
         """
+
+        # Insert the pin.
         self.insert_into(
             table='pins',
             column=['url', 'title', 'subtitle'],
             value=(data['url'], data['title'], data['subtitle'])
         )
 
+        # Query for the pin id.
         rows = self.select_from(
             column='id', table='pins',
             query={'column': 'url', 'value': data['url']}
         )
         pin_id = [col[0] for col in rows][0]
 
-        # insert the image  FIXME: pins_id to pin_id or pin
+        # Insert the images.
         for image in data['images']:
             self.insert_into(
                 table='images',
-                column=['pins_id', 'url'],
+                column=['pin_id', 'url'],
                 value=(pin_id, image)
             )
 
     def query_urls(self, urls:List[str]):
-        """Select urls matching `urls` sent in the list.
+        """Query the database for matching URLs.
 
         Args:
-            urls: list of urls used for check the url field in the db.
+            urls: The URLs to query the database.
+
         Returns:
-            list: List[str] if any matches, List[] otherwise.
+            List[str]: The `urls` that are already in the database.
         """
+
         rows = self.select_from(
             column='url', table='pins',
             query={'column': 'url', 'value': urls}
@@ -72,12 +96,21 @@ class SQL:
 
 
 class DBConnection:
+    """Manage the connection to the database.
+
+    Args:
+        database: The database. E.g. Postgres, MySQL.
+        conn_params: The connection parameters.
+    """
+
     def __init__(self, database:str, conn_params:Dict[str, Union[int, str]]):
         self.connection = (database, conn_params)
         self.cursor = self._connection
 
     @property
     def connection(self):
+        """The connection to the database."""
+
         return self._connection
 
     @connection.setter
@@ -86,36 +119,52 @@ class DBConnection:
             if conn[0] == 'Postgres':
                 self._connection = psycopg.connect(**conn[1])
             elif conn[0] == 'MySQL':
-                self._connection = mysql.connector.connect(conn[1])
+                self._connection = mysql.connector.connect(**conn[1])
         except Exception as e:
             print(e, '\n[!] Error connecting to the database [!]')
             exit()
 
     @property
     def cursor(self):
+        """The connection's cursor."""
+
         return self._cursor
 
     @cursor.setter
     def cursor(self, connection):
-        if isinstance(connection, psycopg.Connection):
-            self._cursor = connection.cursor()
+        self._cursor = connection.cursor()
 
     def fetchall(self):
-        return self._cursor.fetchall()
+        """Return the rows from the database."""
+
+        return self.cursor.fetchall()
 
     def execute(self, sql:str, params=()):
-        return self.cursor.execute(sql, params)
+        """Execute some SQL statement.
+
+        Args:
+            sql: The SQL statement.
+            params: The values to fill the SQL placeholders.
+        """
+
+        self.cursor.execute(sql, params)
 
     def commit(self):
-        return self._connection.commit()
+        """Commit the database records."""
+
+        self.connection.commit()
 
 
-class Postgres(DBConnection, SQL):
+class Postgres(SQL, DBConnection):
+    """Connect to the Postgres database."""
+
     def __init__(self, conn_params:Dict[str, str]):
         DBConnection.__init__(self, self.__class__.__name__, conn_params)
 
 
 class MySQL(SQL, DBConnection):
+    """Connect to the MySQL database."""
+
     def __init__(self, conn_params:Dict[str, str]):
         DBConnection.__init__(self, self.__class__.__name__, conn_params)
 
