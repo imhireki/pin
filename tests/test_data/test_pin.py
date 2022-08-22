32a477a4d5a78c947a1fb8a2048d79be0c324414
imHireki <imhireki@gmail.com>
2022-08-22 19:49:03 -0300

    [test]: add TestPin

--- tests/test_data/test_pin.py
+++ tests/test_data/test_pin.py
@@ -59,3 +59,42 @@ class TestPinData:
     def test_get_images(self, raw_pin_data):
         pin_data = pin.PinData(self.pin_url)
         assert pin_data._get_images() == [raw_pin_data['images']['736x']['url']]
+
+
+class TestPin:
+    def test_pin_data(self, mocker):
+        pin_data_mock = mocker.patch('data.pin.PinData', return_value=mocker.Mock())
+
+        pin_object = pin.Pin('_')
+        pin_data = pin_object.get_pin_data()
+
+        assert pin_data_mock.call_args.args == ('_',)
+        assert pin_data is pin_data_mock()
+        assert pin_object._pin_data is pin_data_mock()
+
+    def test_fetch_data(self, mocker):
+        data = {"x": 1, "y": 2}
+        mocker.patch('data.pin.PinData')
+
+        pin_object = pin.Pin('_')
+        pin_object._pin_data = mocker.Mock(fetch_data=lambda: data)
+
+        fetched_data = pin_object.fetch_data()
+
+        assert fetched_data == data
+        assert pin_object._fetched_data == data
+
+    @pytest.mark.parametrize('fetched_data,validity', [
+        [{"images": ['img.jpg']}, True],
+        [{"images": []}, False]
+    ])
+    def test_is_valid(self, mocker, fetched_data, validity):
+        mocker.patch('data.pin.PinData')
+
+        pin_object = pin.Pin('_')
+        mocker.patch.object(pin_object, 'fetch_data')
+        pin_object._fetched_data = fetched_data
+
+        assert pin_object.is_valid() is validity
+        assert pin_object.fetch_data.called
+
