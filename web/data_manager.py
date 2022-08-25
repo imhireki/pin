bef004e431970b5649bff7fa9717463fb534f1ed
imHireki <imhireki@gmail.com>
2022-08-25 16:41:29 -0300

    [feat]: add WebElementManager

--- web/data_manager.py
+++ web/data_manager.py
@@ -20,3 +20,16 @@ class IWebDataManager(ABC):
     def make_html_soup(self, html: str) -> BeautifulSoup:
         return BeautifulSoup(html, 'html.parser')
 
+
+class WebElementManager(IWebDataManager):
+    def __init__(self, web_driver: browser.WebDriver) -> None:
+        self._web_driver_wait: WebDriverWait = WebDriverWait(web_driver, 10)
+
+    def get(self, element: str, locator: By = By.CSS_SELECTOR,
+            condition: EC = EC.presence_of_element_located) -> WebElement:
+        return self._web_driver_wait.until(condition(locator, element))
+
+    def get_html(self, web_element: WebElement) -> str:
+        return web_element.get_attribute('outerHTML')
+
+
