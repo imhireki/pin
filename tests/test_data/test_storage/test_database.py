0289a3ac3439fbb979aed1f6b37c2ca08ae13c7b
imHireki <imhireki@gmail.com>
2022-08-27 19:15:33 -0300

    [test]: add test_mysql_connection

--- tests/test_data/test_storage/test_database.py
+++ tests/test_data/test_storage/test_database.py
@@ -13,3 +13,16 @@ def test_postgresql_connection(mocker):
     assert connect_mock.call_args.kwargs == connection_options
     assert connection.connection.close.called
     assert cursor == connection.connection.cursor.return_value
+
+def test_mysql_connection(mocker):
+    connect_mock = mocker.patch('mysql.connector.connect')
+    connection_options = {"database": "database", "password": 123}
+    connection = database.MySQLConnection(**connection_options)
+
+    connection.connect()
+    connection.disconnect()
+    cursor = connection.get_cursor()
+
+    assert connect_mock.call_args.kwargs == connection_options
+    assert connection.connection.close.called
+    assert cursor == connection.connection.cursor.return_value
