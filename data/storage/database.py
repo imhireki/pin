3faa19296f74234934e379a51b6cce8f105b3015
imHireki <imhireki@gmail.com>
2022-08-28 16:29:53 -0300

    [feat]: add PostgreSQLStorage

--- data/storage/database.py
+++ data/storage/database.py
@@ -118,3 +118,11 @@ class IDatabaseStorage(ABC):
             return
         return selected_rows_with_url[0][0]
 
+
+class PostgreSQLStorage(IDatabaseStorage):
+    def __init__(self, database: str, user: str, password: str, host: str, **extra_options) -> None:
+        self._connection = PostgreSQLConnection(
+            dbname=database, user=user, password=password, host=host, **extra_options
+        )
+        self._connection.connect()
+
