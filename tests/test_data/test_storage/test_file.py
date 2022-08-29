f0f187b5a0e545b3b4c001f71f15c31e26055d6d
imHireki <imhireki@gmail.com>
2022-08-29 20:31:59 -0300

    [test]: add TestJsonStorage

--- tests/test_data/test_storage/test_file.py
+++ tests/test_data/test_storage/test_file.py
@@ -20,3 +20,29 @@ class TestCSVStorage:
 
         assert csv_writer_mock.return_value.writerow.call_args.args == (new_pin_data,)
 
+
+class TestJsonStorage:
+    def test_query_pin(self, mocker):
+        stored_data = {"url": "my_pin_url"}
+
+        storage = file.JsonStorage('_')
+        get_json_data_mock = mocker.patch.object(
+            storage, '_get_json_data', return_value=[stored_data]
+        )
+
+        assert storage.query_pin(stored_data['url']) == stored_data['url']
+
+    def test_insert_pin(self, mocker):
+        json_dump_mock = mocker.patch('json.dump')
+        stored_data = {"url": "pin_123"}
+        new_pin_data = {"url": "my_pin_url"}
+
+        storage = file.JsonStorage('_')
+        get_json_data_mock = mocker.patch.object(
+            storage, '_get_json_data', return_value=[stored_data]
+        )
+
+        storage.insert_pin(new_pin_data)
+
+        assert [stored_data, new_pin_data] in json_dump_mock.call_args.args
+
