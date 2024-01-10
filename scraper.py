bae07980643385e4c4f30c2755c0e5a4abb15191
imHireki <imhireki@gmail.com>
2024-01-10 18:41:03 -0300

    [feat]: add try block with finally to close the browser every time

--- scraper.py
+++ scraper.py
@@ -8,27 +8,32 @@ import settings
 
 storage = JsonStorage('data.json')
 
-browser = Firefox(binary="geckodriver", headless=True)
+browser = Firefox(binary="geckodriver", headless=False)
 browser.setup_driver()
 
 pinterest = Pinterest(browser.driver)
 
-browser.get(settings.URLS['LOGIN'])
-pinterest.perform_login()
-time.sleep(3)
+try:
+    browser.get(settings.URLS['LOGIN'])
+    pinterest.perform_login()
+    time.sleep(3)
 
-for query in ['Luffy Icon']:
-    search_url = pinterest.make_search_url(query)
+    for query in ['Luffy Icon']:
+        search_url = pinterest.make_search_url(query)
 
-    browser.get(search_url)
-    browser.scroll_to_page_bottom()
+        browser.get(search_url)
+        browser.scroll_to_page_bottom()
 
-    pins_urls = pinterest.find_pins_urls()
-    pins_data = [pinterest.fetch_pin_data(pin_url) for pin_url in pins_urls]
+        pins_urls = pinterest.find_pins_urls()
+        pins_data = [pinterest.fetch_pin_data(pin_url) for pin_url in pins_urls]
 
-    for data in pins_data:
-        if storage.query_pin(data['url']):
-            continue
-        storage.insert_pin(data)
-
-browser.close()
+        for data in pins_data:
+            if storage.query_pin(data['url']):
+                continue
+            storage.insert_pin(data)
+except KeyboardInterrupt:
+    pass
+except Exception:
+    pass
+finally:
+    browser.close()
