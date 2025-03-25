1e1895f889a954a4623896373adec477600b25da
imhireki <hireki@proton.me>
2025-03-25 12:25:56 -0300

    [test]: remove deprecated make_pin_html and raw_pin_data fixtures

--- tests/test_data/conftest.py
+++ tests/test_data/conftest.py
@@ -25,15 +25,6 @@ def pin_data():
     }
 
 
-@pytest.fixture
-def raw_pin_data():
-    return {
-        **base_pin_data,
-        "carousel_data": {},
-        "images": {"736x": {"url": "img.jpg"}},
-    }
-
-
 @pytest.fixture(scope="session")
 def cookies():
     browser = Firefox(binary="geckodriver", headless=True)
@@ -46,21 +37,3 @@ def cookies():
     cookies = browser.driver.get_cookies()
     browser.quit()
     return cookies
-
-
-@pytest.fixture
-def make_pin_html():
-    def _make_pin_html(raw_pin_data):
-        pin_id = "123"
-        script_data = {"initialReduxState": {"pins": {pin_id: raw_pin_data}}}
-
-        html_script_data_json_string = json.dumps(script_data)
-
-        html_script = f"""
-            <script id='__PWS_INITIAL_PROPS__' type='application/json'>
-                {html_script_data_json_string}
-            </script>
-        """
-        return html_script
-
-    return _make_pin_html
