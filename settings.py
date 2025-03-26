73eac529135b9edca927afe8fe693862e9e717cd
imhireki <hireki@proton.me>
2025-03-26 19:41:23 -0300

    [refactor]: make it simpler to get my elements from settings.

--- settings.py
+++ settings.py
@@ -3,6 +3,7 @@ import os
 from selenium.webdriver.common.by import By
 from dotenv import load_dotenv
 
+
 load_dotenv()
 
 URLS = {
@@ -11,33 +12,20 @@ URLS = {
     "HOME": "https://www.pinterest.com",
 }
 
-
 ELEMENTS = {
-    "GOOGLE_LOGIN": {
-        "element": "#credential_picker_container > iframe:nth-child(1)",
-        "locator": By.CSS_SELECTOR,
-    },
-    "GOOGLE_LOGIN_CLOSE_BUTTON": {
-        "element": "close",
-        "locator": By.ID,
-    },
-    "PINS": {
-        "element": "div.vbI:nth-child(1)",
-        "locator": By.CSS_SELECTOR,
-    },
-    "EMAIL_INPUT": {
-        "element": "#email",
-        "locator": By.CSS_SELECTOR,
-    },
-    "PASSWORD_INPUT": {
-        "element": "#password",
-        "locator": By.CSS_SELECTOR,
-    },
+    "GOOGLE_LOGIN": (
+        By.CSS_SELECTOR,
+        "#credential_picker_container > iframe:nth-child(1)",
+    ),
+    "GOOGLE_LOGIN_CLOSE_BUTTON": (By.ID, "close"),
+    "PINS": (By.CSS_SELECTOR, "div.vbI:nth-child(1)"),
+    "EMAIL_FIELD": (By.CSS_SELECTOR, "#email"),
+    "PASSWORD_FIELD": (By.CSS_SELECTOR, "#password"),
 }
 
 CREDENTIALS = {
-    "EMAIL": os.getenv("PINTEREST_EMAIL"),
-    "PASSWORD": os.getenv("PINTEREST_PASSWORD"),
+    "EMAIL": os.getenv("PINTEREST_EMAIL", ""),
+    "PASSWORD": os.getenv("PINTEREST_PASSWORD", ""),
 }
 
 DATABASE = {
