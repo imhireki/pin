b7065f7a809ff11dcb37d2c10337ffe273f70a45
imhireki <hireki@proton.me>
2025-04-01 15:14:12 -0300

    [feat]: add WebPage interface and the Login subclass

--- pinterest/webpages.py
+++ pinterest/webpages.py
@@ -1,3 +1,4 @@
+from abc import ABC, abstractmethod
 import time
 import re
 
@@ -10,6 +11,67 @@ from requests import Session
 from pinterest.pin import Pin, PinData
 from browser import IBrowser
 import settings
+
+
+class WebPage(ABC):
+    def __init__(self, browser: IBrowser) -> None:
+        self._browser = browser
+
+    def go_to_page(self) -> None:
+        self._browser.get(self.url)
+
+    @property
+    @abstractmethod
+    def url(self) -> str:
+        pass
+
+
+class Login(WebPage):
+    def __init__(self, browser: IBrowser):
+        self.session = Session()
+        super().__init__(browser)
+
+    @property
+    def url(self) -> str:
+        return settings.URLS["LOGIN"]
+
+    def close_google_login(self) -> None:
+        self._browser.wait.until(
+            EC.frame_to_be_available_and_switch_to_it(settings.ELEMENTS["GOOGLE_LOGIN"])
+        )
+
+        close_button = self._browser.wait.until(
+            EC.element_to_be_clickable(settings.ELEMENTS["GOOGLE_LOGIN_CLOSE_BUTTON"])
+        )
+        close_button.click()
+
+        self._browser.switch_to_default_content()
+
+    def authenticate_session(self, retries: int, retry_delay: int) -> None:
+        for _ in range(retries):
+            auth_cookie = self._browser.get_cookie("_auth")
+            if auth_cookie and auth_cookie["value"] == "1":
+                for cookie in self._browser.get_cookies():
+                    self.session.cookies.set(cookie["name"], cookie["value"])
+                return
+            time.sleep(retry_delay)
+
+        raise Exception("Failed to authenticate Session")
+
+    def authenticate(self) -> None:
+        email_field = self._browser.wait.until(
+            EC.element_to_be_clickable(settings.ELEMENTS["EMAIL_FIELD"])
+        )
+
+        email_field.send_keys(settings.CREDENTIALS["EMAIL"])
+
+        password_field = self._browser.wait.until(
+            EC.element_to_be_clickable(settings.ELEMENTS["PASSWORD_FIELD"])
+        )
+
+        password_field.send_keys(settings.CREDENTIALS["PASSWORD"], Keys.ENTER)
+
+        self.authenticate_session(10, 1)
 
 
 class Pinterest:
