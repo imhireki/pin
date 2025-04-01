8fc26f87aa421a7887da715435856d1e643ee9c8
imhireki <hireki@proton.me>
2025-04-01 15:41:13 -0300

    [feat]: add the store_valid_pin util

---
+++ utils.py
@@ -0,0 +1,20 @@
+from requests import Session
+from pinterest.pin import Pin, PinData
+from storage.database import IDatabaseStorage
+from storage.file import IFileStorage
+
+
+type Storage = IFileStorage | IDatabaseStorage
+
+
+def store_valid_pin(storage: Storage, session: Session, pin_id: str) -> None:
+    pin_data = PinData(pin_id, session)
+    pin = Pin(pin_data)
+
+    data = pin.fetch_data()
+
+    if not pin.is_valid() or storage.query_pin(data["url"]):
+        return
+
+    storage.insert_pin(data)
+
