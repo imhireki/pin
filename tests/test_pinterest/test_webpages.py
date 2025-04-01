99f96f761ddef3aaf7ff35ea05409defd6e55919
imhireki <hireki@proton.me>
2025-04-01 15:12:43 -0300

    [test]: prepare tests to add the Login class

--- tests/test_pinterest/test_webpages.py
+++ tests/test_pinterest/test_webpages.py
@@ -1,10 +1,93 @@
 import pytest
 
-from pinterest.webpages import Pinterest
+from pinterest.webpages import Pinterest, Login, SearchFeed
 import settings
 
 
 EC = "selenium.webdriver.support.expected_conditions"
+
+
+class TestLogin:
+    def test_url(self, mocker):
+        login = Login(mocker.Mock())
+
+        assert login.url == "https://www.pinterest.com/login/"
+
+    def test_go_to_page(self, mocker):
+        browser = mocker.Mock()
+
+        login = Login(browser)
+        login.go_to_page()
+
+        browser.get.assert_called_with(login.url)
+
+    def test_close_google_login(self, mocker):
+        frame_to_be_available = mocker.patch(
+            EC + ".frame_to_be_available_and_switch_to_it"
+        )
+        element_to_be_clickable = mocker.patch(EC + ".element_to_be_clickable")
+        browser = mocker.Mock()
+
+        login = Login(browser)
+        login.close_google_login()
+
+        assert browser.wait.until.call_count == 2
+        frame_to_be_available.assert_called_with(settings.ELEMENTS["GOOGLE_LOGIN"])
+        element_to_be_clickable.assert_called_with(
+            settings.ELEMENTS["GOOGLE_LOGIN_CLOSE_BUTTON"]
+        )
+        browser.wait.until().click.assert_called()
+        browser.switch_to_default_content.assert_called()
+
+    @pytest.mark.parametrize(
+        "cookies",
+        [
+            [],
+            [{"name": "_auth", "value": "0"}],
+            [{"name": "_auth", "value": "1"}],
+        ],
+    )
+    def test_authenticate_session(self, mocker, cookies):
+        sleep = mocker.patch("time.sleep")
+        auth_cookie = cookies[0] if cookies else {}
+
+        browser = mocker.Mock()
+        browser.get_cookie.return_value = None if not cookies else auth_cookie
+        browser.get_cookies.return_value = cookies
+
+        login = Login(browser)
+        login.session = mocker.Mock()
+
+        if auth_cookie and auth_cookie["value"] == "1":
+            login.authenticate_session(1, 0)
+            login.session.cookies.set.assert_called()
+            sleep.assert_not_called()
+        else:
+            try:
+                login.authenticate_session(1, 0)
+            except Exception:
+                login.session.cookies.set.assert_not_called()
+                sleep.assert_called()
+
+    def test_authenticate(self, mocker):
+        element_to_be_clickable = mocker.patch(EC + ".element_to_be_clickable")
+
+        browser = mocker.Mock()
+        login = Login(browser)
+        auth_session = mocker.patch.object(login, "authenticate_session")
+        login.authenticate()
+
+        assert browser.wait.until.call_count == 2
+
+        element_to_be_clickable.assert_any_call(settings.ELEMENTS["EMAIL_FIELD"])
+        browser.wait.until().send_keys.assert_any_call(settings.CREDENTIALS["EMAIL"])
+
+        element_to_be_clickable.assert_any_call(settings.ELEMENTS["PASSWORD_FIELD"])
+        browser.wait.until().send_keys.assert_any_call(
+            settings.CREDENTIALS["PASSWORD"], "\ue007"
+        )
+
+        auth_session.assert_called_with(10, 1)
 
 
 class TestPinterest:
