e4269040dbcdb52346417f6d7040d59db033a3df
imhireki <hireki@proton.me>
2025-04-01 15:40:42 -0300

    [test]: prepare to test the store_valid_pin from the utils module

---
+++ tests/test_utils.py
@@ -0,0 +1,26 @@
+import pytest
+
+from utils import store_valid_pin
+
+
+@pytest.mark.parametrize("validity, data, is_stored", [
+    [True, {"url": "url"}, False],
+    [True, {"url": "url"}, True],
+    [False, {}, False],
+])
+def test_store_valid_pins(mocker, validity, data, is_stored):
+    storage = mocker.Mock()
+    storage.query_pin.return_value = None if not is_stored else "url"
+
+    mocker.patch("utils.PinData")
+    pin = mocker.patch("utils.Pin").return_value
+    pin.is_valid.return_value = validity
+    pin.fetch_data.return_value = data
+
+    store_valid_pin(storage, mocker.Mock(), "123")
+
+    if not validity or is_stored:
+        storage.insert_pin.assert_not_called()
+    else:
+        storage.insert_pin.assert_called_with(data)
+
