dd1a984b41aabf8f0ea7aa49905db8780c656f4d
imhireki <hireki@proton.me>
2025-04-11 16:28:16 -0300

    [refactor]: move webdriver-related code back to a simple, functional code

--- browser.py
+++ browser.py
@@ -1,12 +1,6 @@
-from abc import ABC, abstractmethod
-from typing import Any
 import time
-import os
 
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.remote.webdriver import WebDriver
-from selenium.webdriver.common.options import ArgOptions
 from selenium.webdriver import (
     Firefox as FirefoxDriver,
     Chrome as ChromeDriver,
@@ -14,111 +8,25 @@ from selenium.webdriver import (
     ChromeOptions,
 )
 
-import settings
+
+def Firefox(headless=True) -> FirefoxDriver:
+    options = FirefoxOptions()
+    if headless:
+        options.add_argument("-headless")
+    return FirefoxDriver(options)
 
 
-class IBrowser[Opts: ArgOptions](ABC):
-    _default_options: dict[str, Any] = {"headless": True, "wait_timeout": 10}
-    _driver: WebDriver
-    _wait: WebDriverWait
-    _options: dict[str, Any]
+def Chrome(headless=True) -> ChromeDriver:
+    options = ChromeOptions()
 
-    def setup_driver(self) -> None:
-        options = {**self._default_options}
-        options.update(**self._options)
+    if headless:
+        options.add_argument("--headless=new")
 
-        driver_options = self._set_driver_options(options)
-        wait_options = self._set_wait_options(options)
-
-        self._driver = self._set_driver(driver_options)
-        self._wait = WebDriverWait(self._driver, **wait_options)
-
-    @property
-    def driver(self) -> WebDriver:
-        return self._driver
-
-    @property
-    def wait(self) -> WebDriverWait:
-        return self._wait
-
-    @abstractmethod
-    def _set_driver_options(self, options: dict[str, Any]) -> Opts:
-        pass
-
-    @staticmethod
-    def _set_wait_options(options: dict) -> dict:
-        return {"timeout": options["wait_timeout"]}
-
-    @abstractmethod
-    def _set_driver(self, driver_options: Opts) -> WebDriver:
-        pass
-
-    def scroll_down(self, times=1, delay=3) -> None:
-        self._wait.until(
-            EC.visibility_of_element_located(settings.ELEMENTS["PINS"])
-        )
-
-        for _ in range(times):
-            self._driver.execute_script(
-                "window.scrollTo(0, document.body.scrollHeight);"
-            )
-            time.sleep(delay)
-
-    def get_cookie(self, name: str) -> dict:
-        return self._driver.get_cookie(name) or {}
-
-    def get_cookies(self) -> list[dict]:
-        return self._driver.get_cookies()
-
-    def switch_to_default_content(self) -> None:
-        self._driver.switch_to.default_content()
-
-    def get(self, url: str) -> None:
-        self._driver.get(url)
-
-    def quit(self) -> None:
-        self._driver.quit()
+    # Keeps the driver from closing by itself (like firefox)
+    options.add_experimental_option("detach", True)
+    return ChromeDriver(options)
 
 
-class Chromium(IBrowser):
-    _driver_data_directory: str = "/tmp/pin/driver_data/chrome"
-
-    def __init__(self, **options: Any) -> None:
-        self._options: dict[str, Any] = options
-
-    def _set_driver_options(self, options: dict[str, Any]) -> ChromeOptions:
-        driver_options = ChromeOptions()
-
-        if options["headless"]:
-            driver_options.add_argument("--headless=new")
-
-        # Keeps the driver from closing by itself (like firefox)
-        driver_options.add_experimental_option("detach", True)
-
-        driver_options.add_argument(f"--user-data-dir={self._driver_data_directory}")
-        return driver_options
-
-    def _set_driver(self, driver_options: ChromeOptions) -> ChromeDriver:
-        return ChromeDriver(options=driver_options)
-
-
-class Firefox(IBrowser):
-    _driver_data_directory: str = "/tmp/pin/driver_data/firefox"
-
-    def __init__(self, **options: Any) -> None:
-        self._options: dict[str, Any] = options
-
-    def _set_driver_options(self, options: dict[str, Any]) -> FirefoxOptions:
-        driver_options = FirefoxOptions()
-
-        if options["headless"]:
-            driver_options.add_argument("-headless")
-
-        if not os.path.exists(self._driver_data_directory):
-            os.makedirs(self._driver_data_directory)
-
-        driver_options.profile = self._driver_data_directory
-        return driver_options
-
-    def _set_driver(self, driver_options: FirefoxOptions) -> FirefoxDriver:
-        return FirefoxDriver(options=driver_options)
+def scroll_down(web_driver: WebDriver, loading_delay=3) -> None:
+    web_driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    time.sleep(loading_delay)
