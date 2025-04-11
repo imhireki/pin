c0c2e83ec544ddcdd4955f1afb1958d3aaec3b6c
imhireki <hireki@proton.me>
2025-04-11 16:43:28 -0300

    [refactor]: replace try/except with a context manager

--- example.py
+++ example.py
@@ -1,7 +1,7 @@
-from pinterest.webpages import Login, SearchFeed
-from storage.file import CSVStorage, JsonStorage
+from storage.connection import connect_to_postgres, connect_to_mysql
 from storage.database import PostgreSQLStorage, MySQLStorage
-from storage.utils import connect_to_postgres, connect_to_mysql
+from storage.file import CSVStorage, JsonStorage
+from pinterest.webpages import Login, SearchFeed
 from browser import Firefox
 import settings
 import utils
@@ -9,25 +9,22 @@ import utils
 
 QUERY = "Frieren Icon"
 
-db_connection = connect_to_postgres(**settings.DATABASE)
-storage = PostgreSQLStorage(db_connection)
+with (
+    Firefox(headless=True) as web_driver,
+    connect_to_postgres(**settings.DATABASE) as db_connection,
+):
+    storage = PostgreSQLStorage(db_connection)
 
-browser = Firefox(headless=True)
-browser.setup_driver()
-
-try:
-    login = Login(browser)
+    login = Login(web_driver)
     login.go_to_page()
+    login.close_google_login()
     login.authenticate()
     session = login.session
 
-    search_feed = SearchFeed(browser, QUERY)
+    search_feed = SearchFeed(web_driver, QUERY)
     search_feed.go_to_page()
     search_feed.load_more()
     pin_ids = search_feed.find_pin_ids()
 
     for pid in pin_ids:
         utils.store_valid_pin(storage, session, pid)
-finally:
-    browser.quit()
-    db_connection.close()
