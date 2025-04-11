d1c20522f1e113c6487d3f2d0cc6cabf0a529e27
imhireki <hireki@proton.me>
2025-04-11 16:30:06 -0300

    [test]: prepare code WebPage to handle a raw web_driver instead of browser

--- tests/test_pinterest/test_webpages.py
+++ tests/test_pinterest/test_webpages.py
@@ -14,30 +14,31 @@ class TestLogin:
         assert login.url == "https://www.pinterest.com/login/"
 
     def test_go_to_page(self, mocker):
-        browser = mocker.Mock()
-
-        login = Login(browser)
+        web_driver = mocker.Mock()
+        login = Login(web_driver)
         login.go_to_page()
 
-        browser.get.assert_called_with(login.url)
+        web_driver.get.assert_called_with(login.url)
 
     def test_close_google_login(self, mocker):
         frame_to_be_available = mocker.patch(
             EC + ".frame_to_be_available_and_switch_to_it"
         )
         element_to_be_clickable = mocker.patch(EC + ".element_to_be_clickable")
-        browser = mocker.Mock()
+        web_driver = mocker.Mock()
 
-        login = Login(browser)
+        login = Login(web_driver)
+        web_driver_wait = mocker.patch.object(login, "_web_driver_wait")
         login.close_google_login()
 
-        assert browser.wait.until.call_count == 2
+        assert web_driver_wait.until.call_count == 2
+
         frame_to_be_available.assert_called_with(settings.ELEMENTS["GOOGLE_LOGIN"])
         element_to_be_clickable.assert_called_with(
             settings.ELEMENTS["GOOGLE_LOGIN_CLOSE_BUTTON"]
         )
-        browser.wait.until().click.assert_called()
-        browser.switch_to_default_content.assert_called()
+        web_driver_wait.until().click.assert_called()
+        web_driver.switch_to.default_content.assert_called()
 
     @pytest.mark.parametrize(
         "cookies",
@@ -51,11 +52,11 @@ class TestLogin:
         sleep = mocker.patch("time.sleep")
         auth_cookie = cookies[0] if cookies else {}
 
-        browser = mocker.Mock()
-        browser.get_cookie.return_value = None if not cookies else auth_cookie
-        browser.get_cookies.return_value = cookies
+        web_driver = mocker.Mock()
+        web_driver.get_cookie.return_value = None if not cookies else auth_cookie
+        web_driver.get_cookies.return_value = cookies
 
-        login = Login(browser)
+        login = Login(web_driver)
         login.session = mocker.Mock()
 
         if auth_cookie and auth_cookie["value"] == "1":
@@ -72,18 +73,19 @@ class TestLogin:
     def test_authenticate(self, mocker):
         element_to_be_clickable = mocker.patch(EC + ".element_to_be_clickable")
 
-        browser = mocker.Mock()
-        login = Login(browser)
+        web_driver = mocker.Mock()
+        login = Login(web_driver)
+        web_driver_wait = mocker.patch.object(login, "_web_driver_wait")
         auth_session = mocker.patch.object(login, "authenticate_session")
         login.authenticate()
 
-        assert browser.wait.until.call_count == 2
+        assert web_driver_wait.until.call_count == 2
 
         element_to_be_clickable.assert_any_call(settings.ELEMENTS["EMAIL_FIELD"])
-        browser.wait.until().send_keys.assert_any_call(settings.CREDENTIALS["EMAIL"])
+        web_driver_wait.until().send_keys.assert_any_call(settings.CREDENTIALS["EMAIL"])
 
         element_to_be_clickable.assert_any_call(settings.ELEMENTS["PASSWORD_FIELD"])
-        browser.wait.until().send_keys.assert_any_call(
+        web_driver_wait.until().send_keys.assert_any_call(
             settings.CREDENTIALS["PASSWORD"], "\ue007"
         )
 
@@ -104,12 +106,19 @@ class TestSearchFeed:
         browser.get.assert_called_with(search_feed.url)
 
     def test_load_more(self, mocker):
-        browser = mocker.Mock()
+        visibility_of_element_located = mocker.patch(
+            EC + ".visibility_of_element_located"
+        )
+        scroll_down = mocker.patch("pinterest.webpages.scroll_down")
+        web_driver = mocker.Mock()
 
-        search_feed = SearchFeed(browser, "sleep token")
+        search_feed = SearchFeed(web_driver, "sleep token")
+        web_driver_wait = mocker.patch.object(search_feed, "_web_driver_wait")
         search_feed.load_more()
 
-        browser.scroll_down.assert_called()
+        visibility_of_element_located.assert_called_with(settings.ELEMENTS["PINS"])
+        web_driver_wait.until.assert_called()
+        scroll_down.assert_called_with(web_driver, 3)
 
     @pytest.mark.parametrize(
         "html,ids",
@@ -129,7 +138,10 @@ class TestSearchFeed:
         browser = mocker.Mock()
         browser.wait.until.return_value.get_attribute.return_value = html
 
-        search_feed = SearchFeed(browser, "query")
+        search_feed = SearchFeed(mocker.Mock(), "query")
+        web_driver_wait = mocker.patch.object(search_feed, "_web_driver_wait")
+        web_driver_wait.until.return_value.get_attribute.return_value = html
+
         pids = search_feed.find_pin_ids()
 
         visibility_of_element_located.assert_called_with(settings.ELEMENTS["PINS"])
